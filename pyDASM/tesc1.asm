9f4adacfca4edbc2a6bcaeae5653288ef06b9403
Juan Felipe García Catalán <jfgcatalan@gmail.com>
2012-03-30 13:57:31 +0200

    Why is this so hard?

--- pyDASM/tesc1.asm
+++ pyDASM/tesc1.asm
@@ -1,2 +1,4 @@
-             set a, 0xbeef                        ; Assign 0xbeef to register a
-             set [0x1000], a                      ; Assign memory at 0x1000 to value of register a
+             set a, 0xbeef                        
+             set [0x1000], aca
+:aca
+             dat 0xbeef                    
